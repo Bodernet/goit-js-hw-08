3e5a937bf42400cf8bb4016446a17dc57741f09f
Bodernet <142262665+Bodernet@users.noreply.github.com>
2023-12-23 16:18:02 +0200

    старт

--- js/task-03.js
+++
@@ -1,6 +0,0 @@
-const nameInput = document.getElementById("name-input");
-const nameOutput = document.getElementById("name-output");
-
-nameInput.addEventListener("input", (e) => {
-  nameOutput.textContent = e.target.value.trim() || "Anonymous";
-});
